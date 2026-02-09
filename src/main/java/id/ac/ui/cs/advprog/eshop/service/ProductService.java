e8af12b3e49e461302563150844e53bcd598dc44
unknown <ifadhil63@gmail.com>
2026-02-09 20:41:11 +0700

    Finished edit function

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -2,8 +2,11 @@ package id.ac.ui.cs.advprog.eshop.service;
 
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
+import java.util.UUID;
 
 public interface ProductService {
     public Product create(Product product);
     public List<Product> findAll();
+    public Product findById(String id);
+    public Product edit(Product product);
 }
